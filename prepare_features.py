c2cc65184003a76b63ac68fde0cd925c7e390e3b
Iver Martinsen <iver.martinsen@outlook.com>
2024-11-26 17:17:41 +0100

    add argparse, remove imagefolder

--- prepare_features.py
+++ prepare_features.py
@@ -9,15 +9,12 @@ import vision_transformer as vits
 from torchvision import transforms as pth_transforms
 from hdf5_dataloader_v2 import HDF5Dataset
 
-data_path = "100K_BENCHMARK_224x224.hdf5"
-pretrained_weights = '/Users/ima029/Desktop/dino-v1/dino/trained_models/LUMI/zip scrapings (huge)/dino-v1-8370959/checkpoint.pth'
-output_fname = "100K_BENCHMARK_224x224_features.hdf5"
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, default="./hdf5/6407_6-5 1200 mDC.hdf5")
+parser.add_argument("--pretrained_weights", type=str, default='/Users/ima029/Desktop/dino-v1/dino/trained_models/LUMI/zip scrapings (huge)/dino-v1-8370959/checkpoint.pth')
+args = parser.parse_args()
 
-
-
-imagefolder = '/Users/ima029/Desktop/NO 6407-6-5/labelled imagefolders/imagefolder_20/'
-output_fname = 'labelled_crops_features.hdf5'
-
+output_fname = os.path.basename(args.data_path).replace(".hdf5", "_features.hdf5")
 
 transform = pth_transforms.Compose([
     pth_transforms.Resize((256, 256), interpolation=3),
@@ -28,12 +25,12 @@ transform = pth_transforms.Compose([
 
 model = vits.__dict__["vit_small"](patch_size=16, num_classes=0, img_size=[224])
 
-vit_utils.load_pretrained_weights(model, pretrained_weights, "teacher", "vit_small", 16)
+vit_utils.load_pretrained_weights(model, args.pretrained_weights, "teacher", "vit_small", 16)
 model.eval()
 model.cuda()
 
-ds = HDF5Dataset(data_path, transform=transform)
-ds = torchvision.datasets.ImageFolder('/Users/ima029/Desktop/NO 6407-6-5/labelled imagefolders/imagefolder_20/', transform=transform)
+ds = HDF5Dataset(args.data_path, transform=transform)
+#ds = torchvision.datasets.ImageFolder('/Users/ima029/Desktop/NO 6407-6-5/labelled imagefolders/imagefolder_20/', transform=transform)
 
 data_loader = torch.utils.data.DataLoader(
     ds, 
@@ -42,22 +39,19 @@ data_loader = torch.utils.data.DataLoader(
     num_workers=10,)
 
 filenames = [f[0] for f in ds.samples]
-labels = [f[1] for f in ds.samples]
-
-
 filenames = [str(f) for f in filenames]
+labels = [f[1] for f in ds.samples]
 
 features = []
 
 for i, (samples, _) in enumerate(data_loader):
     print(f"Batch {i+1}/{len(data_loader)}")
-    #samples = samples.cuda()
+    samples = samples.cuda()
     features.append(model(samples).detach().cpu().numpy())
-    #filenames.append(_)
 
 features = np.concatenate(features, axis=0)
 
 with h5py.File(output_fname, "w") as f:
     f.create_dataset("features", data=features)
-    f.create_dataset("filenames", data=filenames[:len(features)])
-    f.create_dataset("labels", data=labels[:len(features)])
+    f.create_dataset("filenames", data=filenames)
+    f.create_dataset("labels", data=labels)
