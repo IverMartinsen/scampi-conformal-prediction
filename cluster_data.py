995055a74a808418b7ab2c8cca376dbdb2bb5810
Iver Martinsen <iver.martinsen@outlook.com>
2024-11-26 19:44:14 +0100

    many changes

--- cluster_data.py
+++ cluster_data.py
@@ -40,17 +40,18 @@ def init_centroids_semi_supervised(x_lab, y_lab, x_un, k):
     return centroids, cluster_labs
 
 
-path_to_features = "100K_BENCHMARK_224x224_features.hdf5"
-path_to_images = "100K_BENCHMARK_224x224.hdf5"
+path_to_features = "./features/6407_6-5 1290 mDC_features.hdf5"
+path_to_images = "./hdf5/6407_6-5 1290 mDC.hdf5"
 
 with h5py.File(path_to_features, 'r') as f:
     filenames = f["filenames"][()]
     features = f["features"][()]
 
+
 filenames = [f.decode() for f in filenames]
 
-with h5py.File(path_to_images, 'r') as f:
-    keys = list(f.keys())
+#with h5py.File(path_to_images, 'r') as f:
+#    keys = list(f.keys())
 
 #images = []
 #
@@ -62,11 +63,11 @@ with h5py.File(path_to_images, 'r') as f:
 #            image = Image.fromarray(image)
 #        images.append(image)
 
-
-kmeans = MiniBatchKMeans(n_clusters=1000, random_state=0)
-kmeans.fit(features)
-kmeans.labels_
-
+#
+#kmeans = MiniBatchKMeans(n_clusters=1000, random_state=0)
+#kmeans.fit(features)
+#kmeans.labels_
+#
 
 path_to_labelled_features = "labelled_crops_features.hdf5"
 with h5py.File(path_to_labelled_features, 'r') as f:
@@ -78,13 +79,13 @@ lab_filenames = [f.decode() for f in lab_filenames]
 
 x_lab = lab_features[np.where(lab_labels == 11)]
 y_lab = lab_labels[np.where(lab_labels == 11)]
-y_lab = lab_labels
-y_lab = np.arange(50)
 f_lab = np.array(lab_filenames)[np.where(lab_labels == 11)]
+#y_lab = lab_labels
+y_lab = np.arange(50)
 
-random_idx = np.random.choice(range(features.shape[0]), 100)
-x_un = features[random_idx]
-f_un = np.array(filenames)[random_idx]
+random_idx = np.random.choice(range(features.shape[0]), 10000)
+x_un = features #[random_idx]
+f_un = np.array(filenames)#[random_idx]
 
 centroids, cluster_labs = init_centroids_semi_supervised(x_lab, y_lab, x_un, 100)
 
@@ -94,40 +95,71 @@ x_tot = np.concatenate([x_lab, x_un], axis=0)
 
 
 #kmeans = MiniBatchKMeans(n_clusters=100, random_state=0, init=centroids)
-kmeans = KMeans(n_clusters=100, random_state=1, init=centroids)
-kmeans.fit(np.concatenate([x_lab, x_un], axis=0))
+kmeans = KMeans(n_clusters=100, random_state=0, init=centroids)
+kmeans.fit(x_tot)
 kmeans.labels_.shape
 
 
-# update labels
-kmeans.labels_[:50] = cluster_labels
-# update centroids
-for i in range(100):
-    kmeans.cluster_centers_[i] = np.mean(x_tot[kmeans.labels_ == i], axis=0)
-# update labels
-kmeans.labels_ = kmeans.predict(x_tot)
+
+# recluster group 80
+shifted_labels = kmeans.labels_[50:]
+idx = np.where(shifted_labels == 80)[0]
+
+centroids, cluster_labs = init_centroids_semi_supervised(x_lab, y_lab, x_un[idx], 2)
+kmeans = KMeans(n_clusters=2, random_state=0, init=centroids)
+kmeans.fit(np.concatenate([x_lab, x_un[idx]], axis=0))
+kmeans.labels_[50:]
+
+## update labels
+#kmeans.labels_[:50] = cluster_labels
+## update centroids
+#for i in range(100):
+#    kmeans.cluster_centers_[i] = np.mean(x_tot[kmeans.labels_ == i], axis=0)
+## update labels
+#kmeans.labels_ = kmeans.predict(x_tot)
+
+kmeans.labels_[:50]
 
 
 
 
+#centroids = kmeans.cluster_centers_
+#centroids[0] = np.mean(x_lab, axis=0)
 
-
-centroids = kmeans.cluster_centers_
-centroids[0] = np.mean(x_lab, axis=0)
-
-
+(euclidean_distances(x_lab) + np.eye(50) * 40).max()
 
 D = euclidean_distances(x_lab, features)
-nn = np.argmin(D, axis=1)
+nn = np.argsort(D, axis=1)[:, :1]
 
-idx = np.unique(nn)
+shifted_labels = kmeans.labels_[50:]
 
-for i in nn:
-    plt.imshow(Image.open(f"{path_to_images}/{filenames[i]}"))
-    plt.show()
-    plt.close()
+b = np.where(D[:, np.where(shifted_labels == 80)[0]])[1]
+
+a = np.where(shifted_labels == 80)[0][b]
+
+a = np.unique(a)
+
+idx = a
+
+idx = np.unique(np.where(D < 45)[1])
+
+np.stack(shifted_labels, 50)
+
+idx, counts = np.unique(nn, return_counts=True)
+idx = idx[counts > 1]
 
 
+for i in idx:
+    fname = f_un[i]
+    with h5py.File(path_to_images, 'r') as f:
+        image = f[fname][()]
+        image = read_fn(image)
+        image = Image.fromarray(image)
+    image.show()
+
+
+plt.hist(D.flatten(), bins=100)
+plt.savefig("hist.png")
 
 kmeans = KMeans(n_clusters=2, random_state=0)
 kmeans.fit(x_lab)
@@ -135,7 +167,7 @@ y_lab = kmeans.labels_
 
 
 
-for i in range(5):
+for i in range(1):
     fnames = f_lab[kmeans.labels_ == i]
     fig, ax = plt.subplots(10, 10, figsize=(20, 20))
     for j, ax in enumerate(ax.flatten()):
@@ -161,14 +193,18 @@ kmeans.labels_[:50]
 
 
 path_to_images = '/Users/ima029/Desktop/NO 6407-6-5/100K_BENCHMARK_224x224/images'
+(kmeans.labels_ == 80).sum()
 
-
-for i in [0, 1, 42, 97]:
+for i in [0, 47]:
     filenames_ = f_un[kmeans.labels_[50:] == i]
     fig, ax = plt.subplots(10, 10, figsize=(20, 20))
     for j, ax in enumerate(ax.flatten()):
         try:
-            img = Image.open(f"{path_to_images}/{filenames_[j]}")
+            with h5py.File(path_to_images, 'r') as f:
+                img = f[filenames_[j]][()]
+                img = read_fn(img)
+                img = Image.fromarray(img)
+            #img = Image.open(f"{path_to_images}/{filenames_[j]}")
             ax.imshow(img)
         except IndexError:
             pass
