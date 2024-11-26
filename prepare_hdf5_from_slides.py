7ad95466b9fcc17c5ccd3d038b66f37b8ca11ea0
Iver Martinsen <iver.martinsen@outlook.com>
2024-11-26 17:00:43 +0100

    add jpg compression with torch and remove storing metadata

--- prepare_hdf5_from_slides.py
+++ prepare_hdf5_from_slides.py
@@ -10,16 +10,22 @@ import tensorflow as tf
 from tqdm import tqdm
 from openslide import OpenSlide, deepzoom
 from utils import get_crop, compute_iou, get_boxes_yolo, get_boxes_thresh
+import torch
+import torchvision
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--path_to_slides", type=str, default="./data/NO 6407-6-5/mrxs")
-parser.add_argument("--destination", type=str, default="./data/hdf5")
+parser.add_argument("--destination", type=str, default="./hdf5")
 parser.add_argument("--method", type=str, default="yolov5")
 parser.add_argument("--image_shape", type=tuple, default=(224, 224, 3))
 args = parser.parse_args()
 
 
 slides = glob.glob(args.path_to_slides + "/*.mrxs")
+
+print(f"Found {len(slides)} slides.")
+
 model = yolov5.load("keremberke/yolov5m-aerial-sheep")  # pretrained model for sheep detection
 
 
@@ -75,7 +81,7 @@ if __name__ == "__main__":
         metadata = []
         
         for row in tqdm(range(num_rows), total=num_rows):
-            for col in tqdm(range(num_cols), total=num_cols):
+            for col in range(num_cols):
                 tile = tile_generator.get_tile(tile_generator.level_count - 1, (row, col))
                 boxes = get_boxes(tile)
                 # only keep non-overlapping boxes
@@ -89,7 +95,10 @@ if __name__ == "__main__":
                     boxes = [box for i, box in enumerate(boxes) if np.all(iou[i] < 1e-3)]
                 for box in boxes:
                     crop = get_crop(box, tile, pad_image=False) # numpy array of variable resolution
-                    crop = tf.io.encode_jpeg(crop)
+                    crop = torchvision.io.encode_jpeg(torch.tensor(crop).permute(2, 0, 1))
+                    # convert to uint8
+                    #crop = tf.convert_to_tensor(crop, dtype=tf.uint8)
+                    #print(crop)
                     #crop = tf.image.resize(crop, args.image_shape[:2])
                     #crop = tf.cast(crop, tf.uint8)
                     
@@ -102,7 +111,8 @@ if __name__ == "__main__":
         #write_slide_to_hdf5(crops, metadata, file_name)
         with h5py.File(file_name, "w") as file:
             for i, (crop, meta) in enumerate(zip(crops, metadata)):
-                file.create_dataset(str(meta), data=crop)
+                file.create_dataset(f"{os.path.basename(slide).split('.')[0]}_crop_{i}", data=crop, dtype='uint8')
+                #file.create_dataset(f"{os.path.basename(slide).split('.')[0]}_meta_{i}", data=str(meta))
         
         end = time.time()
         
