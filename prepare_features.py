1ab85c52d5687f3316e6f993a92c94d510387a6b
Iver Martinsen <iver.martinsen@outlook.com>
2024-12-02 10:15:14 +0100

    change src to folder instead of file

--- prepare_features.py
+++ prepare_features.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import argparse
 import h5py
 import torch
@@ -14,9 +15,6 @@ parser.add_argument("--data_path", type=str, default="./hdf5/6407_6-5 1200 mDC.h
 parser.add_argument("--pretrained_weights", type=str, default='/Users/ima029/Desktop/dino-v1/dino/trained_models/LUMI/zip scrapings (huge)/dino-v1-8370959/checkpoint.pth')
 args = parser.parse_args()
 
-output_fname = os.path.basename(args.data_path).replace(".hdf5", "_features.hdf5")
-output_fname = os.path.join("./features", output_fname)
-
 transform = pth_transforms.Compose([
     pth_transforms.Resize((256, 256), interpolation=3),
     pth_transforms.CenterCrop(224),
@@ -30,30 +28,44 @@ vit_utils.load_pretrained_weights(model, args.pretrained_weights, "teacher", "vi
 model.eval()
 model.cuda()
 
-ds = HDF5Dataset(args.data_path, transform=transform)
-#ds = torchvision.datasets.ImageFolder('/Users/ima029/Desktop/NO 6407-6-5/labelled imagefolders/imagefolder_20/', transform=transform)
+file_paths = glob.glob(args.data_path + "/*.hdf5")
+file_paths.sort()
 
-data_loader = torch.utils.data.DataLoader(
-    ds, 
-    batch_size=128, 
-    shuffle=False,
-    num_workers=10,)
+for path in file_paths:
+    output_fname = os.path.basename(path).replace(".hdf5", "_features.hdf5")
+    output_fname = os.path.join("./features", output_fname)
+    if os.path.exists(output_fname):
+        print(f"Skipping {path}")
+        continue
+    
+    print(f"Processing {path}")
+    
+    ds = HDF5Dataset(path, transform=transform)
+    #ds = torchvision.datasets.ImageFolder('/Users/ima029/Desktop/NO 6407-6-5/labelled imagefolders/imagefolder_20/', transform=transform)
 
-filenames = [f[0] for f in ds.samples]
-filenames = [str(f) for f in filenames]
-labels = [f[1] for f in ds.samples]
-labels = [str(f) for f in labels]
+    data_loader = torch.utils.data.DataLoader(
+        ds, 
+        batch_size=128, 
+        shuffle=False,
+        num_workers=10,)
 
-features = []
+    filenames = [f[0] for f in ds.samples]
+    filenames = [str(f) for f in filenames]
+    labels = [f[1] for f in ds.samples]
+    labels = [str(f) for f in labels]
 
-for i, (samples, _) in enumerate(data_loader):
-    print(f"Batch {i+1}/{len(data_loader)}")
-    samples = samples.cuda()
-    features.append(model(samples).detach().cpu().numpy())
+    features = []
 
-features = np.concatenate(features, axis=0)
+    for i, (samples, _) in enumerate(data_loader):
+        print(f"Batch {i+1}/{len(data_loader)}")
+        samples = samples.cuda()
+        features.append(model(samples).detach().cpu().numpy())
 
-with h5py.File(output_fname, "w") as f:
-    f.create_dataset("features", data=features)
-    f.create_dataset("filenames", data=filenames)
-    f.create_dataset("labels", data=labels)
+    features = np.concatenate(features, axis=0)
+
+    with h5py.File(output_fname, "w") as f:
+        f.create_dataset("features", data=features)
+        f.create_dataset("filenames", data=filenames)
+        f.create_dataset("labels", data=labels)
+
+print("Done.")
