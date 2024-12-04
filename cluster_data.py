8535b5652b7a6d1ed59a3f60e921f65faeac6167
Iver Martinsen <iver.martinsen@outlook.com>
2024-12-04 15:43:22 +0100

    some changes

--- cluster_data.py
+++ cluster_data.py
@@ -5,10 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from PIL import Image
-from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import euclidean_distances
 from utils import read_fn
-
 
 
 def init_centroids_semi_supervised(x_lab, y_lab, x_un, k):
@@ -20,6 +19,8 @@ def init_centroids_semi_supervised(x_lab, y_lab, x_un, k):
         x_un (_type_): features of unlabeled data
         k (_type_): number of clusters
     """
+    rng = np.random.default_rng(seed=42)
+    
     k_lab = np.unique(y_lab).shape[0]
     k_un = k - k_lab
 
@@ -31,100 +32,176 @@ def init_centroids_semi_supervised(x_lab, y_lab, x_un, k):
         centroids[i] = np.mean(x_lab[y_lab == lab], axis=0)
         cluster_labs[y_lab == lab] = i
 
+    # fill the rest of the centroids with the first centroid
+    for i in range(k_lab, k):
+        centroids[i] = centroids[0]
     
     for i in tqdm(range(k_un), total=k_un):
         d = euclidean_distances(x_un, centroids).min(axis=1)
         p = d / d.sum()
-        centroids[k_lab + i] = x_un[np.random.choice(range(x_un.shape[0]), p=p)]
+        centroids[k_lab + i] = x_un[rng.choice(range(x_un.shape[0]), p=p)]
     
     return centroids, cluster_labs
 
 
-path_to_features = "./features/6407_6-5 1290 mDC_features.hdf5"
-path_to_images = "./hdf5/6407_6-5 1290 mDC.hdf5"
+def load_hdf5(path):
+    with h5py.File(path, 'r') as f:
+        filenames = f["filenames"][()]
+        features = f["features"][()]
+        labels = f["labels"][()]
 
-with h5py.File(path_to_features, 'r') as f:
-    filenames = f["filenames"][()]
-    features = f["features"][()]
+    filenames = np.array([f.decode() for f in filenames])
+    labels = np.array([int(f.decode()) for f in labels])
+    return filenames, features, labels
 
 
-filenames = [f.decode() for f in filenames]
+# load unlabelled features
+path_to_features = '/Users/ima029/Desktop/NO 6407-6-5/features/6407_6-5 2020 mDC_features.hdf5'
+path_to_images = "./hdf5/6407_6-5 2020 mDC.hdf5"
 
-#with h5py.File(path_to_images, 'r') as f:
-#    keys = list(f.keys())
+f_un, x_un, _ = load_hdf5(path_to_features)
 
-#images = []
-#
-#for i, key in enumerate(keys):
-#    if key in filenames:
-#        with h5py.File(path_to_images, 'r') as f:
-#            image = f[key][()] # fancy indexing to get the value of the dataset
-#            image = read_fn(image)
-#            image = Image.fromarray(image)
-#        images.append(image)
+import joblib
 
-#
-#kmeans = MiniBatchKMeans(n_clusters=1000, random_state=0)
-#kmeans.fit(features)
-#kmeans.labels_
-#
+ood_detector = joblib.load("ood_detection_model.pkl")
 
+y_pred = ood_detector.predict(x_un)
+
+int_to_class = {
+    0: "ok",
+    1: "black",
+    2: "dark",
+    3: "blurry",
+    4: "artifact",
+    5: "multi",
+}
+
+for i in np.unique(y_pred):
+    fnames = f_un[y_pred == i]
+    # random sample
+    fnames = np.random.choice(fnames, 100, replace=False)
+    fig, ax = plt.subplots(10, 10, figsize=(20, 20))
+    for j, ax in enumerate(ax.flatten()):
+        try:
+            with h5py.File(path_to_images, 'r') as f:
+                img = f[fnames[j]][()]
+                img = read_fn(img)
+                img = Image.fromarray(img)
+                img = img.resize((224, 224))
+            #img = Image.open(f"{path_to_images}/{filenames_[j]}")
+            ax.imshow(img)
+
+
+        except IndexError:
+            pass
+        ax.axis("off")
+    fig.suptitle(f"Group {int_to_class[i]}", fontsize=20)
+    plt.savefig(f"Group_{int_to_class[i]}.png")
+    plt.close()
+
+x_un = x_un[y_pred == 0]
+f_un = f_un[y_pred == 0]
+
+np.unique(y_pred, return_counts=True)
+
+
+# load labelled features
 path_to_labelled_features = "labelled_crops_features.hdf5"
-with h5py.File(path_to_labelled_features, 'r') as f:
-    lab_features = f["features"][()]
-    lab_labels = f["labels"][()]
-    lab_filenames = f["filenames"][()]
 
-lab_filenames = [f.decode() for f in lab_filenames]
+lab_filenames, lab_features, lab_labels = load_hdf5(path_to_labelled_features)
 
-x_lab = lab_features[np.where(lab_labels == 11)]
-y_lab = lab_labels[np.where(lab_labels == 11)]
-f_lab = np.array(lab_filenames)[np.where(lab_labels == 11)]
-#y_lab = lab_labels
-y_lab = np.arange(50)
+from sklearn.linear_model import LogisticRegression
 
-random_idx = np.random.choice(range(features.shape[0]), 10000)
-x_un = features #[random_idx]
-f_un = np.array(filenames)#[random_idx]
+model = LogisticRegression(max_iter=1000, random_state=0)
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=20)
+model.fit(lab_features, lab_labels)
 
-centroids, cluster_labs = init_centroids_semi_supervised(x_lab, y_lab, x_un, 100)
+model.score(lab_features, lab_labels)
+
+joblib.dump(model, "genus_classifier.pkl")
 
 
+class_of_interest = 14
 
+f_lab = np.array(lab_filenames)[np.where(lab_labels == class_of_interest)]
+x_lab = lab_features[np.where(lab_labels == class_of_interest)]
+y_lab = lab_labels[np.where(lab_labels == class_of_interest)]
+
+ood_detector = joblib.load("ood_detection_model.pkl")
+a = ood_detector.predict(lab_features)
+
+b = lab_filenames[a == 5]
+
+for file in b:
+    print(file)
+
+np.unique(a, return_counts=True)
+
+# concatenate features
 x_tot = np.concatenate([x_lab, x_un], axis=0)
 
+# init centroids
+centroids, cluster_labs = init_centroids_semi_supervised(x_lab, y_lab, x_un, 100)
 
-#kmeans = MiniBatchKMeans(n_clusters=100, random_state=0, init=centroids)
 kmeans = KMeans(n_clusters=100, random_state=0, init=centroids)
 kmeans.fit(x_tot)
-kmeans.labels_.shape
+
+shifted_labels = kmeans.labels_[len(x_lab):]
+
+filenames_ = f_un[shifted_labels == 0]
+
+a = model.predict_proba(x_un[shifted_labels == 0])
+
+a = model.predict_proba(lab_features)
+
+# compute entropy
+entropy = -np.sum(a * np.log(a), axis=1)
+entropy.mean()
+
+len(filenames_)
+
+df = pd.DataFrame({"filename": filenames_, "label": [class_of_interest] * len(filenames_)})
+df.to_csv(f"./feature_stats/{os.path.basename(path_to_features).replace('_features.hdf5', '.csv')}", index=False)
+
+for file in filenames_:
+    with h5py.File(path_to_images, 'r') as f:
+        img = f[file][()]
+        img = read_fn(img)
+        img = Image.fromarray(img)
+        os.makedirs("./extracted_crops/inaperturopollenites", exist_ok=True)
+        img.save(f"./extracted_crops/inaperturopollenites/{file}.png")
 
 
 
-# recluster group 80
-shifted_labels = kmeans.labels_[50:]
-idx = np.where(shifted_labels == 80)[0]
+for i in [0]:
+    fig, ax = plt.subplots(10, 10, figsize=(20, 20))
+    for j, ax in enumerate(ax.flatten()):
+        try:
+            with h5py.File(path_to_images, 'r') as f:
+                img = f[filenames_[j]][()]
+                img = read_fn(img)
+                img = Image.fromarray(img)
+            #img = Image.open(f"{path_to_images}/{filenames_[j]}")
+            ax.imshow(img)
+        except IndexError:
+            pass
+        ax.axis("off")
+    fig.suptitle(f"Group {i}", fontsize=20)
+    plt.savefig(f"Group_{i}.png")
+    plt.close()
 
-centroids, cluster_labs = init_centroids_semi_supervised(x_lab, y_lab, x_un[idx], 2)
-kmeans = KMeans(n_clusters=2, random_state=0, init=centroids)
-kmeans.fit(np.concatenate([x_lab, x_un[idx]], axis=0))
-kmeans.labels_[50:]
 
-## update labels
-#kmeans.labels_[:50] = cluster_labels
-## update centroids
-#for i in range(100):
-#    kmeans.cluster_centers_[i] = np.mean(x_tot[kmeans.labels_ == i], axis=0)
-## update labels
-#kmeans.labels_ = kmeans.predict(x_tot)
+
+
+
+
 
 kmeans.labels_[:50]
 
 
 
 
-#centroids = kmeans.cluster_centers_
-#centroids[0] = np.mean(x_lab, axis=0)
 
 (euclidean_distances(x_lab) + np.eye(50) * 40).max()
 
@@ -195,23 +272,6 @@ kmeans.labels_[:50]
 path_to_images = '/Users/ima029/Desktop/NO 6407-6-5/100K_BENCHMARK_224x224/images'
 (kmeans.labels_ == 80).sum()
 
-for i in [0, 47]:
-    filenames_ = f_un[kmeans.labels_[50:] == i]
-    fig, ax = plt.subplots(10, 10, figsize=(20, 20))
-    for j, ax in enumerate(ax.flatten()):
-        try:
-            with h5py.File(path_to_images, 'r') as f:
-                img = f[filenames_[j]][()]
-                img = read_fn(img)
-                img = Image.fromarray(img)
-            #img = Image.open(f"{path_to_images}/{filenames_[j]}")
-            ax.imshow(img)
-        except IndexError:
-            pass
-        ax.axis("off")
-    fig.suptitle(f"Group {i}", fontsize=20)
-    plt.savefig(f"Group_{i}.png")
-    plt.close()
 
 for i in range(100):
     x = (kmeans.labels_[50:] == i).sum()
