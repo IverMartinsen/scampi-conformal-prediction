cf7d35f3127fbd0f3b0a632b2887dd70c88db95b
Iver Martinsen <iver.martinsen@outlook.com>
2024-12-11 10:32:58 +0100

    add argparse and tfrecords

--- preprocessing/count_all_crops.py
+++ preprocessing/count_all_crops.py
@@ -1,20 +1,38 @@
 import os
+import sys
+
+sys.path.append(".")
+
 import glob
 import h5py
+import argparse
 import numpy as np
 import pandas as pd
-from hdf5_dataloader_v2 import HDF5Dataset
+import tensorflow as tf
+from vit.hdf5_dataloader_v2 import HDF5Dataset
 
-path = "hdf5"
+parser = argparse.ArgumentParser(description='path')
+parser.add_argument('--path', type=str, default="preprocessing/hdf5/", help='path to tfrecords')
+parser.add_argument('--filetype', type=str, default="hdf5", help='filetype')
+args = parser.parse_args()
 
-files = glob.glob(path + "/*.hdf5")
-files.sort()
+if __name__ == "__main__":
 
-counts = {}
+    files = glob.glob(args.path + f"/*.{args.filetype}")
+    files.sort()
 
-for file in files:
-    ds = HDF5Dataset(file)
-    counts[os.path.basename(file)] = len(ds)
+    counts = {}
 
-df = pd.DataFrame(counts.items(), columns=["filename", "count"])
-df.to_csv("counts.csv", index=False)
+    for file in files:
+        if args.filetype == "hdf5":
+            ds = HDF5Dataset(file)
+            counts[os.path.basename(file)] = len(ds)
+        elif args.filetype == "tfrecords":
+            ds = tf.data.TFRecordDataset(file)
+            i = 0
+            for _ in ds:
+                i += 1
+            counts[os.path.basename(file)] = i
+
+    df = pd.DataFrame(counts.items(), columns=["filename", "count"])
+    df.to_csv("counts.csv", index=False)
