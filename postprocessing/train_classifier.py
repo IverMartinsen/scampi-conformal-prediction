3bf7fca61a685be024732459312bb0594ac6e184
Iver Martinsen <iver.martinsen@outlook.com>
2024-12-12 15:10:14 +0100

    add weight decay, class balance, and save entropy and classification report

--- postprocessing/train_classifier.py
+++ postprocessing/train_classifier.py
@@ -9,11 +9,15 @@ import argparse
 import vit.vit_utils as vit_utils
 import torchvision
 import numpy as np
+import pandas as pd
 import vit.vision_transformer as vits
 from time import time
 from PIL import Image
 from torchvision import transforms
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.model_selection import train_test_split, StratifiedKFold
+print(torchvision.__version__)
 
 
 parser = argparse.ArgumentParser()
@@ -24,32 +28,46 @@ parser.add_argument("--pretrained_weights", type=str, default="")
 parser.add_argument("--epochs", type=int, default=100)
 parser.add_argument("--data_dir", type=str, default="data/labelled imagefolders/imagefolder_20")
 parser.add_argument("--num_workers", type=int, default=4)
+parser.add_argument("--weight_decay", type=float, default=0.0)
 args = parser.parse_args()
 
 
 # load the data
 print("Loading data...")
 transform = transforms.Compose([
+    transforms.ColorJitter(brightness=0.0, contrast=0.0, saturation=0.0, hue=0.1),
     transforms.RandomResizedCrop(224, scale=(0.5, 1.0), interpolation=Image.BICUBIC),
     transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
     vit_utils.GaussianBlur(p=0.5),
     transforms.ToTensor(),
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
+#test_transform = transforms.Compose([
+#    transforms.Resize(256),
+#    transforms.CenterCrop(224),
+#    transforms.ToTensor(),
+#    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+#])
+
 dataset = torchvision.datasets.ImageFolder(args.data_dir, transform=transform)
 
 i_tr, i_val = train_test_split(np.arange(len(dataset)), test_size=0.2, random_state=42, stratify=dataset.targets)
 
-x_tr = torch.utils.data.DataLoader(
-    torch.utils.data.Subset(dataset, i_tr),
-    batch_size=args.batch_size, 
-    shuffle=True, 
+x_tr, x_val = torch.utils.data.Subset(dataset, i_tr), torch.utils.data.Subset(dataset, i_val)
+#x_tr.dataset.transform, x_val.dataset.transform = transform, transform
+
+tr_loader = torch.utils.data.DataLoader(
+    x_tr,
+    batch_size=args.batch_size,
+    shuffle=True,
     num_workers=args.num_workers,
     pin_memory=True,
 )
-x_val = torch.utils.data.DataLoader(
-    torch.utils.data.Subset(dataset, i_val),
+
+val_loader = torch.utils.data.DataLoader(
+    x_val,
     batch_size=args.batch_size,
     shuffle=False,
     num_workers=args.num_workers,
@@ -133,17 +151,52 @@ def train_model(model, x_tr, loss_fn, optimizer, num_epochs, x_val=None):
         loss, acc, t = round(loss, 4), round(acc, 4), round(t, 4)
         if x_val is not None:
             test_loss, test_acc = test_model(model, x_val, loss_fn)
-            val_msg = f", Test Loss: {test_loss}, Test Accuracy: {test_acc}"
+            val_msg = f", Test Loss: {round(test_loss, 4)}, Test Accuracy: {round(test_acc, 4)}"
         else:
             val_msg = ""
         print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss}, Accuracy: {acc}, Time: {t}" + val_msg)
 
 if __name__ == "__main__":
     
-    loss_fn = torch.nn.CrossEntropyLoss()
+    weights = compute_class_weight(
+        "balanced", classes=np.unique(x_tr.dataset.targets), y=x_tr.dataset.targets,
+        )
+    
+    loss_fn = torch.nn.CrossEntropyLoss(weight=torch.tensor(weights, dtype=torch.float32).to(args.device))
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
-    train_model(model, x_tr, loss_fn, optimizer, args.epochs, x_val)
+    train_model(model, tr_loader, loss_fn, optimizer, args.epochs, val_loader)
 
     torch.save(classifier.state_dict(), "classifier.pth")
+    
+    model.eval()
+    
+    y_true = []
+    y_pred = []
+    y_entr = []
+    with torch.no_grad():
+        for _ in range(10): # augment the data with 10 random crops
+            for x, y in val_loader:
+                x = x.to(args.device)
+                y = y.to(args.device)
+                logits = model(x)
+                proba = torch.nn.functional.softmax(logits, dim=1)
+                y_true.append(y.cpu().numpy())
+                y_pred.append(logits.argmax(1).cpu().numpy())
+                y_entr.append(-torch.sum(proba * torch.log(proba), dim=1).cpu().numpy())
+    y_true = np.concatenate(y_true)
+    y_pred = np.concatenate(y_pred)
+    y_entr = np.concatenate(y_entr)
+    
+    report = classification_report(
+        y_true, y_pred, target_names=x_val.dataset.classes, output_dict=True,
+        )
+    
+    pd.DataFrame(report).to_csv("classification_report.csv")
+    
+    e = {}
+    for i in range(20):
+        e[x_val.dataset.classes[i]] = y_entr[y_true == i][np.random.choice(len(y_entr[y_true == i]), 40)]
+    
+    pd.DataFrame(e).to_csv("entropy.csv")
