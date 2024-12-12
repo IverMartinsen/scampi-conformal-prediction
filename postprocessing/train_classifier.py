9d16348daa0e77678b58483e4c9d0dc3604c0ba0
Iver Martinsen <iver.martinsen@outlook.com>
2024-12-12 12:09:07 +0100

    add split for validation data

--- postprocessing/train_classifier.py
+++ postprocessing/train_classifier.py
@@ -1,11 +1,19 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), "vit"))
+
 import torch
 import argparse
-import vit_utils
+import vit.vit_utils as vit_utils
 import torchvision
-import vision_transformer as vits
+import numpy as np
+import vit.vision_transformer as vits
 from time import time
 from PIL import Image
 from torchvision import transforms
+from sklearn.model_selection import train_test_split
 
 
 parser = argparse.ArgumentParser()
@@ -14,7 +22,7 @@ parser.add_argument("--lr", type=float, default=1e-4)
 parser.add_argument("--device", type=str, default="cuda")
 parser.add_argument("--pretrained_weights", type=str, default="")
 parser.add_argument("--epochs", type=int, default=100)
-parser.add_argument("--data_dir", type=str, default="labelled imagefolders/imagefolder_20")
+parser.add_argument("--data_dir", type=str, default="data/labelled imagefolders/imagefolder_20")
 parser.add_argument("--num_workers", type=int, default=4)
 args = parser.parse_args()
 
@@ -30,14 +38,26 @@ transform = transforms.Compose([
 ])
 
 dataset = torchvision.datasets.ImageFolder(args.data_dir, transform=transform)
-dataloader = torch.utils.data.DataLoader(
-    dataset, 
+
+i_tr, i_val = train_test_split(np.arange(len(dataset)), test_size=0.2, random_state=42, stratify=dataset.targets)
+
+x_tr = torch.utils.data.DataLoader(
+    torch.utils.data.Subset(dataset, i_tr),
     batch_size=args.batch_size, 
     shuffle=True, 
     num_workers=args.num_workers,
     pin_memory=True,
 )
-print(f"Number of training images: {len(dataset)}")
+x_val = torch.utils.data.DataLoader(
+    torch.utils.data.Subset(dataset, i_val),
+    batch_size=args.batch_size,
+    shuffle=False,
+    num_workers=args.num_workers,
+    pin_memory=True,
+)
+
+print(f"Number of training images: {len(i_tr)}")
+print(f"Number of validation images: {len(i_val)}")
 
 # load the model
 pretrained_model = vits.__dict__["vit_small"](patch_size=16, num_classes=0, img_size=[224])
@@ -92,16 +112,38 @@ def train_one_epoch(model, dataloader, loss_fn, optimizer):
     
     return loss.item(), accuracy, time() - t
 
-def train_model(model, dataloader, loss_fn, optimizer, num_epochs):
+def test_model(model, dataloader, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    test_loss, correct = 0, 0
+    with torch.no_grad(): # disable gradient calculation to speed up computation
+        for x, y in dataloader:
+            x = x.to(args.device)
+            y = y.to(args.device)
+            logits = model(x)
+            test_loss += loss_fn(logits, y).item()
+            correct += (logits.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= size
+    correct /= size
+    return test_loss, correct
+
+def train_model(model, x_tr, loss_fn, optimizer, num_epochs, x_val=None):
     for epoch in range(num_epochs):
-        loss, acc, t = train_one_epoch(model, dataloader, loss_fn, optimizer)
+        loss, acc, t = train_one_epoch(model, x_tr, loss_fn, optimizer)
         loss, acc, t = round(loss, 4), round(acc, 4), round(t, 4)
-        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss}, Accuracy: {acc}, Time: {t}")
+        if x_val is not None:
+            test_loss, test_acc = test_model(model, x_val, loss_fn)
+            val_msg = f", Test Loss: {test_loss}, Test Accuracy: {test_acc}"
+        else:
+            val_msg = ""
+        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss}, Accuracy: {acc}, Time: {t}" + val_msg)
 
-loss_fn = torch.nn.CrossEntropyLoss()
+if __name__ == "__main__":
+    
+    loss_fn = torch.nn.CrossEntropyLoss()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-train_model(model, dataloader, loss_fn, optimizer, args.epochs)
+    train_model(model, x_tr, loss_fn, optimizer, args.epochs, x_val)
 
-torch.save(classifier.state_dict(), "classifier.pth")
+    torch.save(classifier.state_dict(), "classifier.pth")
