04b93a44795b66a176dbbf09d2ee03c942a9e8de
Iver Martinsen <iver.martinsen@outlook.com>
2025-02-13 15:39:47 +0100

    load ref entropy from file instead

--- 4-conformal-prediction/plot_entropy_distribution.py
+++ 4-conformal-prediction/plot_entropy_distribution.py
@@ -9,34 +9,17 @@ parser.add_argument("--alpha", type=float, default=0.05)
 parser.add_argument("--src", type=str, help="Path to the source directory containing the entropy.json files")
 args = parser.parse_args()
 
+args.src = '/Users/ima029/Desktop/NO 6407-6-5/4-conformal-prediction/results/NO 15-9-19 A_alpha_0.5_20250213153206'
+
 f = open(os.path.join(args.src, "entropy.json"))
 entropy_un = json.load(f)
+f.close()
+
+f = open(os.path.join(args.src, "ref_entropy.json"))
+entropy_lab = json.load(f)
+f.close()
+
 lab_to_name = json.load(open(os.path.join(args.src, "lab_to_name.json")))
-
-src_models = [
-    './training/trained_models/20250210152137_seed1',
-    './training/trained_models/20250210155635_seed2',
-    './training/trained_models/20250210163107_seed3',
-    './training/trained_models/20250210170115_seed4',
-    './training/trained_models/20250210173135_seed5',
-    './training/trained_models/20250210180149_seed6',
-    './training/trained_models/20250210183208_seed7',
-    './training/trained_models/20250210190244_seed8',
-    './training/trained_models/20250210193252_seed9',
-    './training/trained_models/20250210200258_seed10',
-]
-
-entropies = []
-
-for path in src_models:
-    with open(os.path.join(path, "entropy.json")) as f:
-        entropies.append(json.load(f))
-
-entropy_lab = {}
-for k in lab_to_name.values():
-    entropy_lab[k] = []
-    for e in entropies:
-        entropy_lab[k] += e[k]
 
 fontsize = 20
 
