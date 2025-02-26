cb76a7be37722e909e6ee2f6c4fd594dd440caa9
Iver Martinsen <iver.martinsen@outlook.com>
2025-02-26 16:17:54 +0100

    add plot for bissacte

--- data/NO 15-9-19 A/ground_truths.py
+++ data/NO 15-9-19 A/ground_truths.py
@@ -37,6 +37,36 @@ ground_truths = {
             3810: 6,
             3816: 1,
         },
+    'bisaccate':
+        {
+            3780: 18,
+            3786: 21,
+            3792: 16,
+            3798: 17,
+            3804: 18,
+            3810: 29,
+            3816: 18,
+            3822: 27,
+            3824: 85,
+            3830: 76,
+            3834: 68,
+            3835: 27,
+            3837: 29,
+            3839: 19,
+            3841: 23,
+            3844: 22,
+            3846: 18,
+            3848: 14,
+            3851: 20,
+            3854: 21,
+            3870: 27,
+            3876: 24,
+            3891: 22,
+            3894: 19,
+            3898: 10,
+            3908: 18,
+            3919: 19,
+        },
 }
 
 for genus in ground_truths.keys():
@@ -46,11 +76,14 @@ for genus in ground_truths.keys():
     fontsize = 18
 
     plt.figure(figsize=(20, 10))
-    plt.bar(x, y, width=5, edgecolor='white')
+    plt.bar(x, y, width=10, edgecolor='white')
     plt.xlim(3760, 3940)
     plt.xticks(np.arange(3760, 3940, 20), rotation=45, fontsize=fontsize)
     plt.ylim(0, 60)
     plt.yticks(np.arange(0, 60, 10), fontsize=fontsize)
+    if genus == 'bisaccate':
+        plt.ylim(0, 100)
+        plt.yticks(np.arange(0, 100, 20), fontsize=fontsize)
     plt.xlabel("Depth", fontsize=18)
     plt.ylabel("Count", fontsize=18)
     plt.tight_layout()
